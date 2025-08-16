ad5789aed49a113174bbc4c8586972c5eb097e81
IgnacioUTN <109432533+Keasenacho@users.noreply.github.com>
2025-08-16 17:35:36 -0300

    Add files via upload

---
+++ Guia1.1Ejerc3/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Ejercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbVerResumen = new System.Windows.Forms.ListBox();
+            this.btnRegistrarPeaje = new System.Windows.Forms.Button();
+            this.btnVerResumen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbVerResumen
+            // 
+            this.lbVerResumen.FormattingEnabled = true;
+            this.lbVerResumen.Location = new System.Drawing.Point(64, 12);
+            this.lbVerResumen.Name = "lbVerResumen";
+            this.lbVerResumen.Size = new System.Drawing.Size(491, 368);
+            this.lbVerResumen.TabIndex = 0;
+            // 
+            // btnRegistrarPeaje
+            // 
+            this.btnRegistrarPeaje.Location = new System.Drawing.Point(564, 78);
+            this.btnRegistrarPeaje.Name = "btnRegistrarPeaje";
+            this.btnRegistrarPeaje.Size = new System.Drawing.Size(116, 23);
+            this.btnRegistrarPeaje.TabIndex = 1;
+            this.btnRegistrarPeaje.Text = "Registrar Peaje";
+            this.btnRegistrarPeaje.UseVisualStyleBackColor = true;
+            this.btnRegistrarPeaje.Click += new System.EventHandler(this.btnRegistrarPeaje_Click);
+            // 
+            // btnVerResumen
+            // 
+            this.btnVerResumen.Location = new System.Drawing.Point(582, 107);
+            this.btnVerResumen.Name = "btnVerResumen";
+            this.btnVerResumen.Size = new System.Drawing.Size(98, 23);
+            this.btnVerResumen.TabIndex = 2;
+            this.btnVerResumen.Text = "VerResumen";
+            this.btnVerResumen.UseVisualStyleBackColor = true;
+            this.btnVerResumen.Click += new System.EventHandler(this.btnVerResumen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerResumen);
+            this.Controls.Add(this.btnRegistrarPeaje);
+            this.Controls.Add(this.lbVerResumen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRegistrarPeaje;
+        private System.Windows.Forms.Button btnVerResumen;
+        public System.Windows.Forms.ListBox lbVerResumen;
+    }
+}
+
---
+++ Guia1.1Ejerc3/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class Form1 : Form
+    {
+        Peaje peaje = new Peaje();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrarPeaje_Click(object sender, EventArgs e)
+        {
+            Resumen ResumenPeaje = new Resumen();
+
+            if (ResumenPeaje.ShowDialog() == DialogResult.OK)
+            {
+                int dia =Convert.ToInt32(ResumenPeaje.tbDias.Text);
+                int cantidadVehiculos = Convert.ToInt32(ResumenPeaje.tbCantidadAutos.Text);
+
+                peaje.RegistrarResumenDia(dia, cantidadVehiculos);
+            }
+        }
+
+        private void btnVerResumen_Click(object sender, EventArgs e)
+        {
+            lbVerResumen.Items.Clear();
+            lbVerResumen.Items.Add("Los días en que ingresaron más autos que el promedio en el mes:");
+            int[] dias = peaje.CalcularLosDiasMayoresAlPromedio(out int cantDias);
+
+            for (int n = 0; n < cantDias; n++)
+            {
+                lbVerResumen.Items.Add($"{dias[n]} (cantidad: {peaje.VerCantidadDeAutosEnUnDia(dias[n])})");
+            }
+
+            lbVerResumen.Items.Add("¿Cuál de los tercios del mes tuvo mayor movimiento?");
+            int tercio = peaje.CalcularTercioDelMesMayor();
+
+            lbVerResumen.Items.Add(tercio);
+        }
+    }
+   }
+
